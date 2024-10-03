98e140336939213092f36b17ab623fc9d312a408
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-10-03 17:51:36 +0200

    Initial Commit

---
+++ SwiftUI_Beginner_Full_Playlist/SwiftUI_Beginner_Full_PlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Beginner_Full_PlaylistApp.swift
+//  SwiftUI_Beginner_Full_Playlist
+//
+//  Created by Manyuchi, Carrington C on 2024/10/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Beginner_Full_PlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
