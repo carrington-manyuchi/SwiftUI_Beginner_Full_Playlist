3a9e78b3dea1c7dcc4a69d1d2bb34442d64ce820
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-10-12 04:58:36 +0200

    FocusState

--- SwiftUI_Beginner_Full_Playlist/FocusStateBootcamp.swift
+++ SwiftUI_Beginner_Full_Playlist/FocusStateBootcamp.swift
@@ -9,8 +9,62 @@
 import SwiftUI
 
 struct FocusStateBootcamp: View {
+    
+    enum OnboardingFields {
+        case username
+        case password
+    }
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    @FocusState private var  fieldInFocus: OnboardingFields?
+    //@FocusState private var passwordInFocus: Bool
+    //@FocusState private var usernameInFocus: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30.0) {
+            TextField("Add your name here...", text: $username)
+               // .focused($usernameInFocus)
+                .focused($fieldInFocus, equals: .username)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.37))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            SecureField("Add your password here...", text: $password)
+               // .focused($passwordInFocus)
+                .focused($fieldInFocus, equals: .password)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.37))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+            
+            Button("SIGN UP") {
+                    
+                let usernameIsValid = !username.isEmpty
+                let passwordIsValid = !password.isEmpty
+                if usernameIsValid && passwordIsValid {
+                    print("Sign UP")
+                } else if usernameIsValid {
+                    fieldInFocus = .password
+//                    usernameInFocus = false
+//                    passwordInFocus = true
+                } else {
+                    fieldInFocus = .username
+//                    usernameInFocus = true
+//                    passwordInFocus = false
+                }
+            }
+        }
+        .padding(40)
+//        .onAppear {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                self.usernameInFocus = true
+//            }
+//        }
     }
 }
 
